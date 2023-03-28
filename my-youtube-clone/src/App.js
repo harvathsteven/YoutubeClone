f5b63db79a53587c15d270649387f31cdb4e1793
harvathsteven <harvathsteven@gmail.com>
2023-03-27 17:06:12 -0700

    First commit, some structure

---
+++ my-youtube-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header />
+            </header>
+            <main className="App-main">
+                {/* Insert main content components here */}
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ my-youtube-clone/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            {/* Insert logo, search bar, and user account options here */}
+        </div>
+    );
+}
+
+export default Header;
